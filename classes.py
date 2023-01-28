dde78b9d2bf02ca29605a7280f932b2ccba17771
technoshed <info@technoshed.uk>
2023-01-28 22:59:27 +0000

    modified:   classes.py More Methods for vehcile class and basic comment class methods devised

--- classes.py
+++ classes.py
@@ -8,10 +8,41 @@ from mysql.connector import Error
 
 # GLOBAL VARIABLES
 
-serverhost = "technoshed.duckdns.org"
+serverhost = "192.168.1.201"
 serveruser = "root"
 serverpass = "TSD704153TSD"
 
+class comment():
+    def __init__(self, ID, REG, DateTime, User, Comment):
+        self.id= ID
+        self.reg= REG
+        self.datetime= DateTime
+        self.user =User
+        self.comment= Comment
+    def add(self):
+        self.datetime = str(datetime.now())
+        # self.user = "sys-commentor"
+        mydb = mysql.connector.connect(host=serverhost,
+                                 user=serveruser,
+                                 database='inspections',
+                                 password=serverpass)
+        mycursor = mydb.cursor()
+        if mydb.is_connected():
+            command ="""INSERT INTO comments VALUES(%s ,%s ,%s, %s ,%s) """
+            input_data = (self.id,self.reg,self.datetime,self.user,self.comment)
+            mycursor.execute(command,input_data)
+            mycursor.close()
+            mydb.commit()
+            mydb.close()
+            return
+
+class inspection():
+    def __init__(self , ID, REG, Inspector, DateTime, Location, Mileage, Front_Image, OS_Image, NS_Image, Rear_Image, Dash_Alerts, Fuel_Level, Oil_Level, Coolant_Level, Brake_Fluid_Level, Steering_Fluid_Level, Screenwash_Level,Hoses,Belts,Tyre_Pressures,NSF_Tyre,OSF_Tyre,OSR_Tyre,NSR_Tyre,Front_Lights,Rear_Lights,Other_Lights,Windscreen_Clear,Horn,Seat_Belts,Wipers,Rear_Wipers,Hand_Brake,Med_Kit,OS_Doors,NS_Doors,Notes,Images,Attachments,PassFail,Filename,Site,AdBlue):
+        self.id =ID
+        self.reg=REG
+        self.inspector=Inspector
+        self.datetime=DateTime
+        pass
 
 class vehicle():
     def __init__(self,ID, REG, VIN, Make, Model, Year, Colour, Mileage, Type, Image, Inspector, DateTime_Updated, Status, Site):
@@ -29,12 +60,12 @@ class vehicle():
         self.updated=DateTime_Updated
         self.status=Status
         self.site=Site
-
     def updateMileage(self, mileage):
-        self.updated = datetime.now()
-        self.inspector = "sys-mileage-upd"
+        self.inspector = "mileage-upd"
         print("MILEAGE-UPD    :"+self.colour+" "+self.make+" "+self.model+"("+self.reg+") from "+ str(self.mileage)+" to "+str(mileage))
+        oldmileage = self.mileage
         self.mileage = mileage
+        self.updated = datetime.now()
         mydb = mysql.connector.connect(host=serverhost,
                                  user=serveruser,
                                  database='inspections',
@@ -46,11 +77,12 @@ class vehicle():
             mycursor.close()
             mydb.commit()
             mydb.close()
+            newcomment = comment("",self.reg,str(self.updated),"upd-mileage","Mileage Updated from "+str(oldmileage)+" to "+str(self.mileage))
+            newcomment.add()
             return
-    
     def writeback(self):
         self.updated = datetime.now()
-        self.inspector = "sys-updater-vehicle"
+        self.inspector = "veh-write"
         mydb = mysql.connector.connect(host=serverhost,
                                  user=serveruser,
                                  database='inspections',
@@ -63,27 +95,20 @@ class vehicle():
             mycursor.close()
             mydb.commit()
             mydb.close()
+            newcomment = comment("",self.reg,str(self.updated),"veh-writeback","Record Updated")
+            newcomment.add()
             print("DB:WRITEBACK   :"+self.colour+" "+self.make+" "+self.model+"("+self.reg+") with "+str(self.mileage)+" mls. Updated @ "+str(self.updated)+" by "+ self.inspector)
             return
-    
-class comment():
-    def __init__(self, ID, REG, DateTime, User, Comment):
-        self.id= ID
-        self.reg= REG
-        self.datetime= DateTime
-        self.user =User
-        self.comment= Comment
-        pass
+    def getMileage(self):
+        return self.mileage
+    def getLastUpdate(self):
+        return self.updated , self.inspector
+    def createComment(self,text):
+        updatetime = str(datetime.now())
 
-    def add(self):
-        pass
-
-class inspection():
-    def __init__(self , ID, REG, Inspector, DateTime, Location, Mileage, Front_Image, OS_Image, NS_Image, Rear_Image, Dash_Alerts, Fuel_Level, Oil_Level, Coolant_Level, Brake_Fluid_Level, Steering_Fluid_Level, Screenwash_Level,Hoses,Belts,Tyre_Pressures,NSF_Tyre,OSF_Tyre,OSR_Tyre,NSR_Tyre,Front_Lights,Rear_Lights,Other_Lights,Windscreen_Clear,Horn,Seat_Belts,Wipers,Rear_Wipers,Hand_Brake,Med_Kit,OS_Doors,NS_Doors,Notes,Images,Attachments,PassFail,Filename,Site,AdBlue):
-        self.id =ID
-        self.reg=REG
-        self.inspector=Inspector
-        self.datetime=DateTime
+        print("creating comment on "+self.colour+" "+ self.make+" "+self.model+"("+self.reg+") : "+str(text))
+        newcomment = comment("",self.reg,str(updatetime),"veh-comment",text)
+        newcomment.add()
         pass
 
 def readVehicle(reg):
@@ -104,7 +129,7 @@ def readVehicle(reg):
         return foundvehicle
 
 van=readVehicle("GD18XOU")
-van.updateMileage(van.mileage+33)
-van.colour="Brown"
-van.status="Fucked a bit more"
-van.writeback()
+#van.createComment("Fuck NIXON chfdhos he is mega bent")
+#van.updateMileage(4000)
+#van.colour="White"
+#van.writeback()
